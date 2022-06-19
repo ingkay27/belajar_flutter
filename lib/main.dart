548b94252d05aecbdaf001598ab719dbe4c4e584
ingkay27 <inkypramudiaramdhani27@gmail.com>
2022-06-19 23:38:30 +0700

    belajar style dan menambahkan font

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,27 @@ void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-        title: Text("My First APP"),
+        title: Text(
+          "My First APP",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            color: Color.fromARGB(255, 238, 238, 238),
+            letterSpacing: 2
+          ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.blue,
       ),
     body: Center(
-      child: Text("hello wordl"),
+      child: Text(
+        "Hello, My Name Is Inky!!!",
+        style: TextStyle(
+          fontSize: 20,
+          letterSpacing: 2,
+          fontFamily: "IndieFlower"
+        ),
+      ),
       ),
     floatingActionButton: FloatingActionButton(onPressed: (){
 
